e74e2a0d8bfbeb7103b0bee8f0318f3886f0f1c2
Andres Felipe -Main <andrefe91@gmail.com>
2023-07-16 21:20:31 -0500

    Start of the logic for the caesar cipher

---
+++ caesar_cipher.rb
@@ -0,0 +1,18 @@
+# Caesar Cipher implementation in Ruby for The Odin Project
+
+#First, we define the configuration array for the replacements
+replacement_array_upcase = ('A'..'Z').zip(1..26).to_h
+replacement_array_downcase = ('a'..'z').zip(1..26).to_h
+replaced
+
+
+def caesar_transformation (phrase, shift_factor)
+
+  phrase_array = phrase.split('')
+
+  phrase_array.map do |letter|
+
+  end
+
+end
+
