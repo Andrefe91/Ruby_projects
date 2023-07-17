32138e32355eb23e002ab16147c58a46bd5352ab
Andres Felipe M <andrefe91@gmail.com>
2023-07-17 13:24:12 -0500

    Basic set up for exercise resolution

---
+++ Sub_String/sub_string.rb
@@ -0,0 +1,9 @@
+
+def count_sub_string (string, dictionary)
+  p string
+  p dictionary
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+p count_sub_string("below", dictionary)
