bf734c323436df07dbba1da4d308cbf49c338cd2
Andres Felipe M <andrefe91@gmail.com>
2023-07-26 12:39:08 -0500

    Finish implementing the main logic and methods for game

--- Project - Tic Tac Toe/tic_tac_toe.rb
+++ Project - Tic Tac Toe/tic_tac_toe.rb
@@ -1,13 +1,17 @@
 # Implementation of the Tic-Tac-Toe game in Ruby for The Odin Project.
 
 class Board
+  attr_accessor :positions, :board
+
   def initialize
     @board = { 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9 }
+    @positions = []
   end
 
   def play(coordinate, symbol)
     unless coordinate.to_int > 10
       @board[coordinate] = symbol
+      track_positions(coordinate)
     end
   end
 
@@ -25,6 +29,10 @@ class Board
       return true if @board[posibility[0]] == @board[posibility[1]] && @board[posibility[1]] == @board[posibility[2]]
     end
     false
+  end
+
+  def track_positions(position)
+    @positions.include?(position) ? nil : @positions.push(position)
   end
 end
 
@@ -59,52 +67,54 @@ def pretty_print(object)
   puts "\n"
 end
 
-def game_start
-
-  tablero = Board.new
-  puts "--------------------------------"
-  puts "Game Starts!!"
-  puts "--------------------------------"
-  print "Please Player 1, enter your name: "
-  name = gets.chomp
-  print "And now, your prefered Symbol: "
-  symbol = gets.chomp
-  player1 = Player.new(name, symbol)
-  puts "--------------------------------"
-  print "Please Player 2, enter your name: "
-  name = gets.chomp
-  print "And now, your prefered Symbol: "
-  symbol = gets.chomp
-  player2 = Player.new(name, symbol)
-  puts "--------------------------------"
-  puts "Ok, Lets Play !!!!!!"
-  puts "--------------------------------"
-  puts 'This is your board:'
-  puts "\n"
-  pretty_print(tablero)
-  puts "\n"
-  game(tablero, player1, player2)
-
-end
-
 def game(tablero, player1, player2)
-
   while true
+    [player1, player2].each do |player|
+      print "It's #{player.name} turn, choose your position: "
+      move = gets.chomp.to_i
 
-    print "It's #{player1.name} turn, choose your position: "
-    move = gets.chomp.to_i
-    tablero.play(move, player1.symbol)
-    pretty_print(tablero)
-    break if tablero.win?
+      if tablero.positions.include?(move)
+        puts "Position already selected, you loose your turn"
+      else
+        tablero.play(move, player.symbol)
+      end
 
-    print "It's #{player2.name} turn, choose your position: "
-    move = gets.chomp.to_i
-    tablero.play(move, player2.symbol)
-    pretty_print(tablero)
-    break if tablero.win?
-
+      pretty_print(tablero)
+      (puts "#{player.name} wins !!") if tablero.win?
+      exit if tablero.win?
+      (puts "It's a tie :/ !!") if tablero.positions.length == 9
+      exit if tablero.positions.length == 9
+    end
   end
-
-
 end
-game_start
+
+
+
+tablero = Board.new
+puts "--------------------------------"
+puts "Game Starts!!"
+puts "--------------------------------"
+print "Please Player 1, enter your name: "
+name = gets.chomp
+print "And now, your prefered Symbol: "
+symbol = gets.chomp
+symbol = symbol.length > 1 ? symbol[0] : symbol
+player1 = Player.new(name, symbol)
+
+puts "--------------------------------"
+print "Please Player 2, enter your name: "
+name = gets.chomp
+print "And now, your prefered Symbol: "
+symbol = gets.chomp
+symbol = symbol.length > 1 ? symbol[0] : symbol
+player2 = Player.new(name, symbol)
+
+puts "--------------------------------"
+puts "Ok, Lets Play !!!!!!"
+puts "--------------------------------"
+puts 'This is your board:'
+puts "\n"
+pretty_print(tablero)
+puts "\n"
+
+game(tablero, player1, player2)
