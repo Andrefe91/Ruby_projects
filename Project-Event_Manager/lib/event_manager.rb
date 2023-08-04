e750ad470ae612f97c161cc7c44446374ed2b2e8
Andres Felipe M <andrefe91@gmail.com>
2023-08-03 19:41:56 -0500

    Logic for cleaning the telephone and registry hours

--- Project-Event_Manager/lib/event_manager.rb
+++ Project-Event_Manager/lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
 
 civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
 civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -44,41 +45,54 @@ def save_thank_you_letter(id, form_letter)
 end
 
 
-def clean_telephone(telephone_number)
-  telephone_number.scan(/\d+/).join('')
+def valid_telephone_number(telephone_number)
+  telephone_number = telephone_number.scan(/\d+/).join('')
+
+  if telephone_number.length == 10
+    telephone_number
+  elsif telephone_number.length == 11 && telephone_number[0] == 1
+    telephone_number[1,telephone_number.length-1]
+  else
+    "invalid"
+  end
 end
 
+def registry_hour(registry)
+  hour = Time.parse(registry.split(' ')[1]).hour
 
+
+
+end
 
 def main
   puts 'Event Manager Initialized!'
+  registry_hours = []
 
   template_letter = File.read('form_letter.erb')
   erb_template = ERB.new template_letter
 
   contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 
-  %{contents.each do |row|
+  contents.each do |row|
     id = row[0]
     name = row[:first_name]
     zipcode = row[:zipcode]
 
     zipcode = clean_zipcode(zipcode)
+    telephone = valid_telephone_number(row[:homephone])
 
-    legislators = legislators_by_zipcode(zipcode)
+    #legislators = legislators_by_zipcode(zipcode)
 
-    form_letter = erb_template.result(binding)
-    save_thank_you_letter(id, form_letter)
+    #form_letter = erb_template.result(binding)
+    #save_thank_you_letter(id, form_letter)
+
+    registry_hours.push(registry_hour(row[:regdate]))
 
 
-  end}
-
-  contents.each do |row|
-    telephone = row[:homephone]
-    puts telephone[0]
-    puts  "#{clean_telephone(telephone)}, length: #{clean_telephone(telephone).length}"
+    p "#{name} | #{registry_hour(row[:regdate])}"
   end
 
+  p registry_hours.tally
 end
 
 main
