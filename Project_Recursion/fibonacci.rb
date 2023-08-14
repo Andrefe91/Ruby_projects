a30ceb91dc74d07e3986f48560ad5d173b339dd0
Andres Felipe M <andrefe91@gmail.com>
2023-08-14 18:35:25 -0500

    Starting logic for the project

---
+++ Project_Recursion/fibonacci.rb
@@ -0,0 +1,15 @@
+#Method for the Fibonacci sequence
+
+def fibs(n)
+  array = [0,1]
+  (n-2).times {|step| array << array[step] + array[step+1] }
+  array
+end
+
+#Method for the Fibonacci sequence with recursive iteration
+
+def fibs_rec(n,array = [0,1])
+  array.length == n ? (return array) : fibs_rec(n,array.append(array[-1] + array[-2]))
+end
+
+p fibs(10000)
