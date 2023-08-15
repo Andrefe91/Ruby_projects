53afc3d6b9d817fa01da35cd491a87024e07653e
Andres Felipe -Main <andrefe91@gmail.com>
2023-08-15 00:04:40 -0500

    Starts the logic for merge sort

--- Project_Recursion/fibonacci.rb
+++ Project_Recursion/fibonacci.rb
@@ -12,4 +12,14 @@ def fibs_rec(n,array = [0,1])
   array.length == n ? (return array) : fibs_rec(n,array.append(array[-1] + array[-2]))
 end
 
-p fibs(10000)
+#p fibs(10000)
+
+
+def divide_array(array)
+
+  return array if array.length/2 < 1
+  p "#{array} and array.length: #{array.length/2}"
+  (array.each_slice((array.length*0.5).round) {|divided| divide_array(divided)})
+end
+
+p divide_array([1,2,3,4,5,6,7])
